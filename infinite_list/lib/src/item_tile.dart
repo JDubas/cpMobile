bbc3bf2a9f31f75e483a03f6b923bbae14df0f66
JDubas <rm76153@fiap.com.br>
2024-10-15 20:54:56 -0300

    bonitando a aplicação

--- infinite_list/lib/src/item_tile.dart
+++ infinite_list/lib/src/item_tile.dart
@@ -1,37 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // Importa o Provider para acesso ao Catalog
+import 'package:provider/provider.dart';
 import 'api/item.dart';
-import 'catalog.dart'; // Importa o Catalog para gerenciar a lista de itens
+import 'catalog.dart';
 
 class ItemTile extends StatelessWidget {
   final Item item;
-  final int index; // Adiciona o índice para remover o item correto
+  final int index;
 
   const ItemTile({required this.item, required this.index, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListTile(
-        leading: AspectRatio(
-          aspectRatio: 1,
-          child: Container(
-            color: item.color,
-          ),
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
         ),
-        title: Text(item.name, style: Theme.of(context).textTheme.titleLarge),
-        trailing: IconButton(
-          icon: const Icon(Icons.close),
-          onPressed: () {
-            // Remove o item do catálogo usando o índice
-            Provider.of<Catalog>(context, listen: false).removeItem(index);
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Tarefa "${item.name}" removida'),
-              ),
-            );
-          },
+        child: ListTile(
+          leading: AspectRatio(
+            aspectRatio: 1,
+            child: Container(
+              color: item.color,
+            ),
+          ),
+          title: Text(
+            item.name,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              Provider.of<Catalog>(context, listen: false).removeItem(index);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('Task "${item.name}" removed'),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
